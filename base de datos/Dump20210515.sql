fdc05b9b2eab800b1bd0d0ea1315404a84843826
oizquierdo873 <oizquierdo873@gmail.com>
2021-05-15 11:48:37 +0200

    [FIX] Modificación base de datos

--- base de datos/Dump20210515.sql
+++ base de datos/Dump20210515.sql
@@ -4,9 +4,7 @@
 -- ------------------------------------------------------
 -- Server version	8.0.19
 drop database if exists jardinrobledo;
-
 create database jardinrobledo;
-
 use jardinrobledo;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -35,19 +33,14 @@ CREATE TABLE `descuentos` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `detallepedido`
+-- Dumping data for table `descuentos`
 --
 
-DROP TABLE IF EXISTS `detallepedido`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `detallepedido` (
-  `id_detallepedidos` int NOT NULL AUTO_INCREMENT,
-  `id_pedidos` varchar(255) DEFAULT NULL,
-  `id_servicios` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id_detallepedidos`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `descuentos` WRITE;
+/*!40000 ALTER TABLE `descuentos` DISABLE KEYS */;
+INSERT INTO `descuentos` VALUES (1,0,5),(2,0,10),(3,0,15);
+/*!40000 ALTER TABLE `descuentos` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `detallepresupuesto`
@@ -69,19 +62,14 @@ CREATE TABLE `detallepresupuesto` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `pedidos`
+-- Dumping data for table `detallepresupuesto`
 --
 
-DROP TABLE IF EXISTS `pedidos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `pedidos` (
-  `id_pedidos` int NOT NULL AUTO_INCREMENT,
-  `fecha_pedido` varchar(255) DEFAULT NULL,
-  `username` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id_pedidos`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `detallepresupuesto` WRITE;
+/*!40000 ALTER TABLE `detallepresupuesto` DISABLE KEYS */;
+INSERT INTO `detallepresupuesto` VALUES (1,1,1);
+/*!40000 ALTER TABLE `detallepresupuesto` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `presupuestos`
@@ -97,11 +85,22 @@ CREATE TABLE `presupuestos` (
   `comentario` varchar(105) DEFAULT NULL,
   `username` varchar(45) DEFAULT NULL,
   `disabled` int DEFAULT NULL,
+  `precio` double DEFAULT NULL,
   PRIMARY KEY (`id_presupuestos`),
   KEY `user_FK_idx` (`username`),
   CONSTRAINT `user_FK` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `presupuestos`
+--
+
+LOCK TABLES `presupuestos` WRITE;
+/*!40000 ALTER TABLE `presupuestos` DISABLE KEYS */;
+INSERT INTO `presupuestos` VALUES (1,500,'2021-05-15T07:27:08.832Z','Terreno sin cesped','@Oscar_bichito',1,45),(5,500,'2021-05-13T21:06:34.328Z','Zonas arcillosas a trata.','@DonOmar',1,5),(10,200,'2021-05-15T07:38:21.096Z','Día soleado','@Usuario',1,45);
+/*!40000 ALTER TABLE `presupuestos` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `servicios`
@@ -119,6 +118,16 @@ CREATE TABLE `servicios` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `servicios`
+--
+
+LOCK TABLES `servicios` WRITE;
+/*!40000 ALTER TABLE `servicios` DISABLE KEYS */;
+INSERT INTO `servicios` VALUES (1,'Sistema de riego',5),(2,'Podas',4),(3,'Cesped artificial',13),(4,'mantenimiento de jardines',7),(5,'Desbrozar',4),(6,'Plantaciones',6);
+/*!40000 ALTER TABLE `servicios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `users`
 --
 
@@ -132,6 +141,16 @@ CREATE TABLE `users` (
   PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES ('@Administrador','ADMINISTRADOR',1),('@Carlos','33',1),('@Cristiano','777777',1),('@CristianoRonaldo','Giorgina',1),('@DonOmar','cantante334',1),('@Fariño_Gomez','we23rt45',1),('@Oscar_bichito','Macarrones',1),('@Pepe','23JOSE12',1),('@Roberto_firmino','Liverrpool',1),('@TohanGomez','234/amorLOVE',1),('@Usuario','SinSal',1),('@Zidane','SoyMadrilista',1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -142,4 +161,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-05-10 12:13:06
+-- Dump completed on 2021-05-15 11:45:51
