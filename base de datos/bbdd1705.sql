995ecb9eaffc3b3ee887a72e1cc167091f2c89a6
oizquierdo873 <oizquierdo873@gmail.com>
2021-05-17 16:59:24 +0200

    [FIX] renovación bbdd

--- base de datos/bbdd1705.sql
+++ base de datos/bbdd1705.sql
@@ -18,9 +18,8 @@
 --
 -- Table structure for table `detallepresupuesto`
 --
-drop database if exists jardinrobledo;
-create database jardinrobledo;
-use jardinrobledo;
+CREATE DATABASE IF NOT EXISTS `jardinrobledo`
+USE `jardinrobledo`;
 DROP TABLE IF EXISTS `detallepresupuesto`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -64,7 +63,7 @@ CREATE TABLE `presupuestos` (
   PRIMARY KEY (`id_presupuestos`),
   KEY `user_FK_idx` (`username`),
   CONSTRAINT `user_FK` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -73,7 +72,7 @@ CREATE TABLE `presupuestos` (
 
 LOCK TABLES `presupuestos` WRITE;
 /*!40000 ALTER TABLE `presupuestos` DISABLE KEYS */;
-INSERT INTO `presupuestos` VALUES (1,500,'2021-05-15T07:27:08.832Z','Terreno sin cesped','@Oscar_bichito',1,45),(5,500,'2021-05-13T21:06:34.328Z','Zonas arcillosas a trata.','@DonOmar',1,5),(10,200,'2021-05-15T07:38:21.096Z','Día soleado','@Usuario',1,45);
+INSERT INTO `presupuestos` VALUES (1,500,'2021-05-16T20:20:21.648Z','ERERE','@Administrador',1,NULL),(5,500,'2021-05-13T21:06:34.328Z','Zonas arcillosas a trata.','@DonOmar',1,5),(10,200,'2021-05-15T07:38:21.096Z','Día soleado','@Usuario',1,45),(13,50,'2021-05-16T20:21:12.312Z','tttttttttttttttttttt','@Administrador',1,NULL),(14,500,'2021-05-16T20:23:09.226Z','232','@Zapatero',1,NULL),(15,50,'2021-05-17T08:31:15.929Z','Para la familia de bayern','@Lewandoski',1,NULL);
 /*!40000 ALTER TABLE `presupuestos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -89,7 +88,7 @@ CREATE TABLE `servicios` (
   `descripcion_servicio` varchar(45) DEFAULT NULL,
   `precio_metro` int DEFAULT NULL,
   PRIMARY KEY (`id_servicios`)
-) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -98,7 +97,7 @@ CREATE TABLE `servicios` (
 
 LOCK TABLES `servicios` WRITE;
 /*!40000 ALTER TABLE `servicios` DISABLE KEYS */;
-INSERT INTO `servicios` VALUES (1,'Sistema de riego',5),(2,'Podas',4),(3,'Cesped artificial',13),(4,'mantenimiento de jardines',7),(5,'Desbrozar',4),(6,'Plantaciones',6);
+INSERT INTO `servicios` VALUES (1,'Sistema de riego',5),(2,'Podas',6),(3,'Cesped artificial',13),(4,'mantenimiento de jardines',7),(5,'Desbrozar',35),(6,'Plantaciones',10);
 /*!40000 ALTER TABLE `servicios` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -123,7 +122,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('@Administrador','ADMINISTRADOR',1),('@Carlos','33',1),('@Cristiano','23%&56',1),('@CristianoRonaldo','Giorgina',1),('@DonOmar','cantante334',1),('@Fariño_Gomez','we23rt45',1),('@Oscar_bichito','Macarrones',1),('@Pepe','23JOSE12',1),('@Roberto_firmino','Liverrpool',1),('@TohanGomez','234/amorLOVE',1),('@Usuario','SinSal',1),('@Zidane','SoyMadrilista',1);
+INSERT INTO `users` VALUES ('@Administrador','yyy',1),('@Aprender','eeee',1),('@Carlos','33',1),('@Cervantes','Quevedo',1),('@Cristiano','23%&56',1),('@CristianoRonaldo','Giorgina',1),('@DonOmar','cantante334',1),('@Fariño_Gomez','we23rt45',1),('@KikeProfesor','Quevedo',1),('@Lewandoski','BayernM',1),('@manolitoGafotas','32333333',1),('@NuevoNombre','1212',1),('@Oscar_bichito','Macarrones',1),('@Pepe','23JOSE12',1),('@Pepita','222',1),('@Prueba','CHICHI',1),('@ROBBIE','eee',1),('@Roberto_firmino','Liverrpool',1),('@Roberto_Seronero','Realpepe',1),('@RubenIzq','333',1),('@titititit','errrrrrrr',1),('@TohanGomez','234/amorLOVE',1),('@Usuario','SinSal',1),('@Zapater','ZAPATO',1),('@Zapatero','23%&56',1),('@Zico','12345',1),('@Zidane','1212',1);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -136,4 +135,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-05-16 12:13:17
+-- Dump completed on 2021-05-17 16:48:25
